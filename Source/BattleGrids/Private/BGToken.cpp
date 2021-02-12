f076302e088c05511ec197d9d59e2dde20834e9a
Matthew Barham <zonks.zonks@gmail.com>
2021-02-12 15:24:54 -0700

    added multicast function of physics and gravity and collision

--- Source/BattleGrids/Private/BGToken.cpp
+++ Source/BattleGrids/Private/BGToken.cpp
@@ -6,12 +6,45 @@
 // Sets default values
 ABGToken::ABGToken()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 	bReplicates = true;
 
 	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
 	RootComponent = StaticMeshComponent;
+	StaticMeshComponent->bIgnoreRadialForce = true;
+	StaticMeshComponent->bIgnoreRadialImpulse = true;
+	StaticMeshComponent->SetLinearDamping(2.f);
+	StaticMeshComponent->SetAngularDamping(2.f);
+}
+
+void ABGToken::ToggleLockTokenInPlace_Implementation(bool bLock)
+{
+	StaticMeshComponent->GetBodyInstance()->bLockXTranslation = bLock;
+	StaticMeshComponent->GetBodyInstance()->bLockYTranslation = bLock;
+	StaticMeshComponent->GetBodyInstance()->bLockZTranslation = bLock;
+	StaticMeshComponent->GetBodyInstance()->bLockXRotation = bLock;
+	StaticMeshComponent->GetBodyInstance()->bLockYRotation = bLock;
+	StaticMeshComponent->GetBodyInstance()->bLockZRotation = bLock;
+	StaticMeshComponent->GetBodyInstance()->SetDOFLock(EDOFMode::SixDOF);
+}
+
+bool ABGToken::GetIsTokenLocked() const
+{
+	auto const Body = StaticMeshComponent->GetBodyInstance();
+	if (Body->bLockXTranslation && Body->bLockYTranslation && Body->bLockZTranslation)
+	{
+		return true;
+	}
+	return false;
+}
+
+void ABGToken::ToggleTokenPhysicsAndCollision_Implementation(bool const bPhysicsOn, bool const bGravityOn,
+                                                             ECollisionEnabled::Type const CollisionType)
+{
+	StaticMeshComponent->SetSimulatePhysics(bPhysicsOn);
+	StaticMeshComponent->SetEnableGravity(bGravityOn);
+	StaticMeshComponent->SetCollisionEnabled(CollisionType);
 }
 
 // Called when the game starts or when spawned
@@ -20,5 +53,4 @@ void ABGToken::BeginPlay()
 	Super::BeginPlay();
 
 	SetReplicateMovement(true);
-	
 }
